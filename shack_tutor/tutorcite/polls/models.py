b5e9879f5c2073fa6b9721ea475915026f85ea71
zvshack <zvshack@knox.edu>
2026-03-03 17:54:47 -0600

    fixes to tutorial

--- shack_tutor/tutorcite/polls/models.py
+++ shack_tutor/tutorcite/polls/models.py
@@ -5,7 +5,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField("date published")
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
