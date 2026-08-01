a88987697abdc3923b53183aac86e90caf36a548
zvshack <zvshack@knox.edu>
2026-07-31 20:08:01 -0500

    fixing static files

--- shack_tutor/tutorcite/shacktutors/settings.py
+++ shack_tutor/tutorcite/shacktutors/settings.py
@@ -124,13 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 
 
 
