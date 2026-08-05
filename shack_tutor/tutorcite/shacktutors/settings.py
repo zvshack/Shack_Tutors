ae28ee60249dbcb22744a70cd0c0cf9af43d0a45
zvshack <zvshack@knox.edu>
2026-08-05 14:27:54 -0500

    fixing email system

--- shack_tutor/tutorcite/shacktutors/settings.py
+++ shack_tutor/tutorcite/shacktutors/settings.py
@@ -146,13 +146,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+EMAIL_TIMEOUT = 10
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
